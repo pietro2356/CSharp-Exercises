ddabe3fe8b931cebb691a809a6be9ee82e036c37
pietro2356 <pietro.rocchio@gmail.com>
2020-06-16 23:19:18 +0200

    Aggiuta la prima pagian di esercizzi da completare.

---
+++ CSharp-Exercises/CADebut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CADebut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharp-Exercises/CLExercises/Generic.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLExercises
+{
+    public class Generic
+    {
+        private static int IVA = 22;
+
+
+        //ES N°1
+        private static double PerimetroTrapRett(double base1, double base2, double altezza)
+        {
+            throw new NotImplementedException();
+        }
+
+        //ES N°2
+        private static double PerimetroTriangRett(double cat1, double cat2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static double AreaTriangRett(double cat1, double cat2)
+        {
+            throw new NotImplementedException();
+        }
+
+        //ES N°3
+        private static double AreaCerchio(double raggio)
+        {
+            throw new NotImplementedException();
+        }
+
+        //ES N°4
+        private static double SecondiDiff(int h1, int min1, int sec1, int h2, int min2, int sec2)
+        {
+            //USARE LA CLASSE DATA PER CALCOLARE LA DIFFERENZA IN MODO PIU' SEMPLICE
+            throw new NotImplementedException();
+        }
+
+        //ES N°5
+        private static double GiorniDiff(int g1, int m1, int a1, int g2, int m2, int a2)
+        {
+            //USARE LA CLASSE DATA PER CALCOLARE LA DIFFERENZA IN MODO PIU' SEMPLICE
+            throw new NotImplementedException();
+        }
+
+        //ES N°6
+        private static double Sconto(double sconto, double prezzo)
+        {
+            throw new NotImplementedException();
+        }
+
+        //ES N°7
+        private static double CalcX(double X)
+        {
+            //EXPRESSION: A * X^2 + B * X -->> A==10; B==15
+            throw new NotImplementedException();
+        }
+
+        //ES N°8
+        public static double CalcMedia()
+        {
+            throw new NotImplementedException();
+        }
+
+        //ES N°9
+        public static double CalcTassaRifiuti(double X, double Y, double P, double m2)
+        {
+            throw new NotImplementedException();
+        }
+
+        //ES N°10
+        public static double CalcSpesa(double costPmetro2, double costHopera, double oreImpiegate)
+        {
+            //Pavimento di 7x4(m)
+            throw new NotImplementedException();
+        }
+
+        //ES N°11
+        public static double SpesaCasetta(int assiL, int assiP, int assiQ, double prezzoL, double prezzoP, double prezzoQ)
+        {
+            //SCONTO 10% - 30€ SPEDIZIONE
+            throw new NotImplementedException();
+        }
+
+        //ES N°12
+        public static double GestionePreventivi(double costoManodoperaOra, double costoRicambi, double ore)
+        {
+            //+ IVA
+            throw new NotImplementedException();
+        }
+
+        //ES N°13
+        private static double GestioneColosseo(int platea, int galleria)
+        {
+            //OUT POSTI LIBERI + TOT INCASSO
+            throw new NotImplementedException();
+        }
+
+        //ES N°14
+        private static double Referendum(int nIscritti, int si, int no, int nulle)
+        {
+            //PERCENTUALE = Nsi / Nvotanti * 100
+            throw new NotImplementedException();
+        }
+
+    }
+}
